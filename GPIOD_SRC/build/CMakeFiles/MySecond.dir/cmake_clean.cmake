6643327e337430616f12413b50a7f834e3c6596d
silver2row <silver1row@cox.net>
2022-03-24 21:46:48 -0500

    organizing!

---
+++ GPIOD_SRC/build/CMakeFiles/MySecond.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MySecond.dir/MySecond.c.o"
+  "CMakeFiles/MySecond.dir/MySecond.c.o.d"
+  "MySecond"
+  "MySecond.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/MySecond.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
