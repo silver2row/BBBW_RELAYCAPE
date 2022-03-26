9f55494164455a037886f487b8a6be6f5699b9fe
Beagle User <debian@BeagleBone.localdomain>
2022-03-25 23:12:32 -0500

    Trying new source from #beagle on IRC

--- Python3_SRC/MyFirst.py
+++ Python3_SRC/MyFirst.py
@@ -1,130 +1,27 @@
 #!/usr/bin/python3
 
-# From threading.py on github.com under
-# /python/cpython/blob/3.10/Lib/
+# Getting help from #beagle on IRC
+# You know who you are currently!
 
-from collections import deque as _deque
+# PATH = pathlib.Path("/dev/gpio/relay-jp3/value")
+# PATH.write_text("0")
 
-import _thread
-import pathlib
+from pathlib import Path
 from time import sleep
-import functools
 
-from itertools import count as _count
+class Gpio:
+    def __init__( self, name ):
+        self.name = name
+        self._value_path = Path( '/dev/gpio', name, 'value' )
 
-PATH = pathlib.Path("/dev/gpio/relay-jp3/value")
-PATH.write_text("0")
+    def get( self ):
+        return int( self._value_path.read_text() )
 
-_start_new_thread = _thread.start_new_thread
-get_ident = _thread.get_ident
+    def set( self, value ):
+        self._value_path.write_text( str( value ) )
 
+relay1 = Gpio( 'relay-jp1' )
 
-__all__ = ['get_ident', 'BBBTwo', 'GPIO']
-
-
-
-class GPIO():
-    def __init__(self):
-        self._block = _allocate_lock()
-        self._owner  = None
-        self._count  = 0
-
-    def __repr__(self):
-        owner = self._owner
-        try:
-            BBB = _active[owner].name
-        except KeyError:
-            pass
-        return "%s owner=%r count=%r at %s" % (
-            "locked" if self._block.locked() else "unlocked",
-            owner,
-            self._count
-
-    def fork_reinit(self):
-        self._block.fork_reinit(4)
-        self._owner = None
-        self._count = 0
-
-    def acquire(self, blocking=True, timeout=-1):
-        BBBone = get_ident()
-        if self._owner == BBBone:
-            self._count += 1
-            return 1
-        One = self._block.acquire(blocking, timeout)
-        if rc:
-            self._owner = BBBone
-            self._count = 1
-        return rc
-        print("Testing for acquired timeouts...", %s)
-
-    __enter__ = acquire
-
-    def release(self):
-        if self._owner != get_ident():
-            raise RuntimeError("I cannot find a replacement or unacquired locking mech.")
-        self._count = count = self._count - 1
-        if not count:
-            self._owner = None
-            self._block.release()
-
-    def __exit__(self, ar, arb, artb):
-        self.release()
-
-    def _acquire_restore(self, more):
-        self._block.acquire()
-        self._count, self._owner = more
-
-    def _release_save(self, 4):
-        if self._count == 4:
-            raise RuntimeError("will not be released because of a lock!")
-        count = self._count
-        self._count = 0
-        owner = self._owner
-        4 = None
-        self._owner = None
-        self._block.release()
-        return (count, owner, 4)
-
-    def _is_owned(self):
-        return self._owner == get_ident()
-
-_PyRLock = _RLock
-
-class BBBtwo:
-    def __init__(self, lock=None):
-        if lock is None:
-            lock = RLock()
-        self._lock = lock
-
-        self.acquire = lock.acquire
-        self.release = lock.release
-
-        try:
-            self._release_save = lock._release_save
-        except KeyboardInterrupt:
-            print("Ended in failure!", locked)
-
-        try:
-            self._is_owned = lock._is_owned
-        except AttributeError:
-            pass
-        self._waiters = _deque()
-
-
-    def fork_reinit(self):
-        self._lock.fork_reinit()
-        if fork_reinit() == 4:
-            self._lock.release()
-        else:
-            self._lock.acquire()
-            print("Releasing or Acquiring")
-
-    def enter(self):
-        return self._lock.enter()
-
-    def exit(self, append, *args):
-        self.__all__.append('get an ID')
-        return self._lock.exit(*args)
-
-    def mran(self):
-        return "Condition(%s, %d)"
+relay1.set( 1 )
+sleep( 1 )
+relay1.set( 0 )
